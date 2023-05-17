562121b978af25af63c9379447038ff72158c0be
example <38441046+armanzhan@users.noreply.github.com>
2023-05-18 00:15:05 +0300

    Update quazi_val.m

--- quazi_val.m
+++ quazi_val.m
@@ -1,3 +1,3 @@
-function [y] = quazi_val(p,q,tau,x)
+function [y] = quazi_val(p,q,tau,x) % аналог функции polyval для квазиполиномов с одним запаздыванием
 y = polyval(p,x) + polyval(q,x) .* exp(tau * x);
 end
